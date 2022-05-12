bd2a94e5d48d0583ddaf41a0c116877ef981ad0a
Benqui <bennyhoyos@hotmail.com>
2022-05-12 13:48:18 -0500

    scrips modif to base

--- scripts/scrip_inyeccion_data.ino
+++ scripts/scrip_inyeccion_data.ino
@@ -5,8 +5,7 @@ void setup() {
   DigiKeyboard.sendKeyStroke(KEY_T,MOD_CONTROL_LEFT|MOD_ALT_LEFT);
   DigiKeyboard.delay(5000);
   
-  //DigiKeyboard.print("sshpass -p 'plicky01' scp benqui@192.168.1.123:~/rob_scripts/robo_script.sh ~/");
-  DigiKeyboard.print("sshpass -p 'plicky01' scp benqui@10.1.105.139:~/scripts/robo.sh ~/");
+  DigiKeyboard.print("sshpass -p 'passwd' scp user@10.1.105.139:~/scripts/robo.sh ~/");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
   
